9063fcdc25ca8e05a93c67d79f8200b8c63312f4
Cameron Grant <cameron.grant2611@gmail.com>
2021-06-18 17:00:01 +0100

    Add java command line args reference

---
+++ Java/CommandLineArguments/src/CommandLineArguments.java
@@ -0,0 +1,19 @@
+public class CommandLineArguments {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Number of arguments provided: " + args.length + "\n");
+
+            System.out.println("Provided arguments: ");
+            for (String arg : args) {
+                System.out.println(arg);
+            }
+
+            System.out.println();
+
+            System.out.println("First argument provided: " + args[0]);
+            System.out.println("Final argument provided: " + args[args.length - 1]);
+        } else {
+            System.out.println("No arguments provided");
+        }
+    }
+}
