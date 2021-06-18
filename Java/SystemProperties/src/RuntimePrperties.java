dd151a12fdeb278ada3ed45d926eebd1bd5e0ed0
Cameron Grant <cameron.grant2611@gmail.com>
2021-06-18 18:34:50 +0100

    Add system properties java reference

---
+++ Java/SystemProperties/src/RuntimePrperties.java
@@ -0,0 +1,26 @@
+public class RuntimePrperties {
+
+    public RuntimePrperties() {}
+
+    public static String getRuntimeVersion() {
+        return Runtime.version().toString();
+    }
+    
+    public static int getNumberofCPUs() {
+        return Runtime.getRuntime().availableProcessors();
+    }
+
+    public static long getTotalRuntimeMemory() {
+        return Runtime.getRuntime().totalMemory();
+    }
+
+    public static long getMaximumRuntimeMemory() {
+        return Runtime.getRuntime().maxMemory();
+    }
+
+    public static long getFreeRuntimeMemory() {
+        return Runtime.getRuntime().freeMemory();
+    }
+
+
+}
