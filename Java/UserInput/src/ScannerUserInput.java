200027f33c53dccf23af4da7179aa644ff17ba57
Cameron Grant <cameron.grant2611@gmail.com>
2021-06-21 15:31:50 +0100

    Change name of directory

---
+++ Java/UserInput/src/ScannerUserInput.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ScannerUserInput {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String userInputString;
+        int userInputInteger;
+
+        System.out.print("Enter string: ");
+        userInputString = reader.nextLine();
+
+        System.out.print("Enter integer: ");
+        userInputInteger = reader.nextInt();
+
+        System.out.println("String entered: " + userInputString);
+        System.out.println("Integer entered: " + userInputInteger);
+
+        reader.close();
+    }
+}
