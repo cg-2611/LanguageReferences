fae9fec359f74c3cfea2cc273f1f93a2072fddce
Cameron Grant <cameron.grant2611@gmail.com>
2021-06-28 17:31:25 +0100

    Update java data structures reference

--- Java/DataStructures/LinkedListExample.java
+++ Java/DataStructures/LinkedListExample.java
@@ -1,47 +1,49 @@
 import java.util.LinkedList;
 
 public class LinkedListExample {
-	public static void main(String[] args) {
-		LinkedList<Integer> linkedList = new LinkedList<Integer>();
 
-		linkedList.add(11);
-		linkedList.add(14);
-		linkedList.add(16);
-		linkedList.add(12);
-		linkedList.add(13);
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
 
-		System.out.print("LinkedList: ");
-		for (int i = 0; i < linkedList.size() ; i++) {
-			System.out.print(linkedList.get(i) + " | ");
-		}
-		System.out.println("\tLinkedList size: " + linkedList.size());
+        linkedList.add(11);
+        linkedList.add(14);
+        linkedList.add(16);
+        linkedList.add(12);
+        linkedList.add(13);
 
-		linkedList.removeLast();  // removes tail of linkedList
-		linkedList.removeFirst(); // removes head of linkedList
+        System.out.print("LinkedList: ");
+        for (int i = 0; i < linkedList.size() ; i++) {
+            System.out.print(linkedList.get(i) + " | ");
+        }
+        System.out.println("\tLinkedList size: " + linkedList.size());
 
-		System.out.print("\nLinkedList: ");
-		for (Integer i : linkedList) {
-			System.out.print(i + " | ");
-		}
-		System.out.println("\tLinkedList size: " + linkedList.size());
+        linkedList.removeLast();  // removes tail of linkedList
+        linkedList.removeFirst(); // removes head of linkedList
 
-		linkedList.addFirst(Integer.valueOf(13)); // add 13 to head of linkedList
-		linkedList.addLast(Integer.valueOf(18));  // add 18 to tail of linkedList
-		linkedList.add(4, Integer.valueOf(11));   // add 11 to index 4 of linkedList
+        System.out.print("\nLinkedList: ");
+        for (Integer i : linkedList) {
+            System.out.print(i + " | ");
+        }
+        System.out.println("\tLinkedList size: " + linkedList.size());
 
-		System.out.print("\nLinkedList: " + linkedList); // calls the toString() method
-		System.out.println("\tLinkedList size: " + linkedList.size() + "\n");
+        linkedList.addFirst(Integer.valueOf(13)); // add 13 to head of linkedList
+        linkedList.addLast(Integer.valueOf(18));  // add 18 to tail of linkedList
+        linkedList.add(4, Integer.valueOf(11));   // add 11 to index 4 of linkedList
 
-		System.out.println("LinkedList contains 9: " + linkedList.contains(Integer.valueOf(9)));
-		System.out.println("LinkedList contains 14: " + linkedList.contains(Integer.valueOf(14)));
-		System.out.println("LinkedList head: " + linkedList.getFirst());
-		System.out.println("LinkedList tail: " + linkedList.getLast());
-		System.out.println("LinkedList empty: " + linkedList.isEmpty());
-		System.out.println("LinkedList index of 16: " + linkedList.indexOf(Integer.valueOf(16)));
-		System.out.println("LinkedList sublist from index 2 to 5: " + linkedList.subList(2, 5));
-		linkedList.clear();
-		System.out.println("\nCleared linkedList\n");
-		System.out.println("LinkedList empty: " + linkedList.isEmpty());
+        System.out.print("\nLinkedList: " + linkedList); // calls the toString() method
+        System.out.println("\tLinkedList size: " + linkedList.size() + "\n");
 
-	}
+        System.out.println("LinkedList contains 9: " + linkedList.contains(Integer.valueOf(9)));
+        System.out.println("LinkedList contains 14: " + linkedList.contains(Integer.valueOf(14)));
+        System.out.println("LinkedList head: " + linkedList.getFirst());
+        System.out.println("LinkedList tail: " + linkedList.getLast());
+        System.out.println("LinkedList empty: " + linkedList.isEmpty());
+        System.out.println("LinkedList index of 16: " + linkedList.indexOf(Integer.valueOf(16)));
+        System.out.println("LinkedList sublist from index 2 to 5: " + linkedList.subList(2, 5));
+        linkedList.clear();
+        System.out.println("\nCleared linkedList\n");
+        System.out.println("LinkedList empty: " + linkedList.isEmpty());
+
+    }
+    
 }
--- Java/DataStructures/StackExample.java
+++ Java/DataStructures/StackExample.java
@@ -1,41 +1,43 @@
 import java.util.Stack;
 
 public class StackExample {
-	public static void main(String[] args) {
-		Stack<Integer> stack = new Stack<Integer>();
 
-		stack.push(122);
-		stack.push(145);
-		stack.push(109);
-		stack.push(156);
-		stack.push(134);
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>();
 
-		System.out.print("Stack: ");
-		for(int i = 0; i < stack.size(); i++) {
-			System.out.print(stack.get(i) + " | ");
-		}
-		System.out.println("\tStack size: " + stack.size());
+        stack.push(122);
+        stack.push(145);
+        stack.push(109);
+        stack.push(156);
+        stack.push(134);
 
-		stack.pop();
-		stack.pop();
+        System.out.print("Stack: ");
+        for(int i = 0; i < stack.size(); i++) {
+            System.out.print(stack.get(i) + " | ");
+        }
+        System.out.println("\tStack size: " + stack.size());
 
-		System.out.print("\nStack: ");
-		for(Integer i : stack) {
-			System.out.print(i + " | ");
-		}
-		System.out.println("\tStack size: " + stack.size());
+        stack.pop();
+        stack.pop();
 
-		stack.pop();
-		stack.push(156);
-		stack.push(117);
+        System.out.print("\nStack: ");
+        for(Integer i : stack) {
+            System.out.print(i + " | ");
+        }
+        System.out.println("\tStack size: " + stack.size());
 
-		System.out.print("\nStack: " + stack); // calls the toString() method
-		System.out.println("\tStack size: " + stack.size() + "\n");
+        stack.pop();
+        stack.push(156);
+        stack.push(117);
 
-		System.out.println("Stack top: " + stack.peek());
-		System.out.println("Stack empty: " + stack.isEmpty());
-		stack.clear();
-		System.out.println("\nCleared stack\n");
-		System.out.println("Stack empty: " + stack.isEmpty());
-	}
+        System.out.print("\nStack: " + stack); // calls the toString() method
+        System.out.println("\tStack size: " + stack.size() + "\n");
+
+        System.out.println("Stack top: " + stack.peek());
+        System.out.println("Stack empty: " + stack.isEmpty());
+        stack.clear();
+        System.out.println("\nCleared stack\n");
+        System.out.println("Stack empty: " + stack.isEmpty());
+    }
+    
 }
