ddbdd580a933bae55f8cd110fc3a05007fc1dd33
Cameron Grant <cameron.grant2611@gmail.com>
2021-06-28 19:46:04 +0100

    Update java networking reference

--- Java/Networking/Client.java
+++ Java/Networking/Client.java
@@ -7,53 +7,53 @@ import java.net.UnknownHostException;
 
 public class Client {
 
-	private Socket serverSocket;
+    private Socket serverSocket;
 
-	public Client(String host, int port) {
-		try {
-			serverSocket = new Socket(host, port);
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public Client(String host, int port) {
+        try {
+            serverSocket = new Socket(host, port);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void start() {
+    public void start() {
 
-		try {
-			BufferedReader userInputReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            BufferedReader userInputReader = new BufferedReader(new InputStreamReader(System.in));
 
-			// create input and output streams for communication
-			// object streams can also be used for transporting serialized objects across network
-			PrintWriter serverOutput = new PrintWriter(serverSocket.getOutputStream(), true);
+            // create input and output streams for communication
+            // object streams can also be used for transporting serialized objects across network
+            PrintWriter serverOutput = new PrintWriter(serverSocket.getOutputStream(), true);
 
-			InputStreamReader serverCharacterStream = new InputStreamReader(serverSocket.getInputStream());
-			BufferedReader serverInput = new BufferedReader(serverCharacterStream);
+            InputStreamReader serverCharacterStream = new InputStreamReader(serverSocket.getInputStream());
+            BufferedReader serverInput = new BufferedReader(serverCharacterStream);
 
-			while (true) {
-				// get input from user and send to server
-				String userInput = userInputReader.readLine();
-				serverOutput.println(userInput);
+            while (true) {
+                // get input from user and send to server
+                String userInput = userInputReader.readLine();
+                serverOutput.println(userInput);
 
-				// get response from server and print to console
-				String serverResponse = serverInput.readLine();
-				System.out.println("Server Response: " + serverResponse);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				serverSocket.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+                // get response from server and print to console
+                String serverResponse = serverInput.readLine();
+                System.out.println("Server Response: " + serverResponse);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	public static void main(String[] args) {
-		Client client = new Client("localhost", 8000);
-		client.start();
-	}
+    public static void main(String[] args) {
+        Client client = new Client("localhost", 8000);
+        client.start();
+    }
 
 }
--- Java/Networking/Server.java
+++ Java/Networking/Server.java
@@ -7,51 +7,52 @@ import java.net.Socket;
 
 public class Server {
 
-	private ServerSocket serverSocket;
+    private ServerSocket serverSocket;
 
-	public Server(int port) {
-		try {
-			serverSocket = new ServerSocket(port);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public Server(int port) {
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void start() {
-		System.out.println("Server listening on port: " + serverSocket.getLocalPort() + "...");
+    public void start() {
+        System.out.println("Server listening on port: " + serverSocket.getLocalPort() + "...");
 
-		try {
-			// accept connection from client
-			Socket clientSocket = serverSocket.accept();
+        try {
+            // accept connection from client
+            Socket clientSocket = serverSocket.accept();
 
-			System.out.println("Accepted connection on: " + serverSocket.getLocalPort() + " ; "+ clientSocket.getPort());
+            System.out.println("Accepted connection on: " + serverSocket.getLocalPort() + " ; "+ clientSocket.getPort());
 
-			// create input and output streams for communication
-			// object streams can also be used for transporting serialized objects across network
-			InputStreamReader clientCharacterStream = new InputStreamReader(clientSocket.getInputStream());
-			BufferedReader clientInput = new BufferedReader(clientCharacterStream);
+            // create input and output streams for communication
+            // object streams can also be used for transporting serialized objects across network
+            InputStreamReader clientCharacterStream = new InputStreamReader(clientSocket.getInputStream());
+            BufferedReader clientInput = new BufferedReader(clientCharacterStream);
 
-			PrintWriter clientOutput = new PrintWriter(clientSocket.getOutputStream(), true);
+            PrintWriter clientOutput = new PrintWriter(clientSocket.getOutputStream(), true);
 
-			while (true) {
-				// echo input from client back to client
-				String clientInputString = clientInput.readLine();
-				clientOutput.println(clientInputString);
-			}
+            while (true) {
+                // echo input from client back to client
+                String clientInputString = clientInput.readLine();
+                clientOutput.println(clientInputString);
+            }
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				serverSocket.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	public static void main(String[] args) {
-		Server server = new Server(8000);
-		server.start();
-	}
+    public static void main(String[] args) {
+        Server server = new Server(8000);
+        server.start();
+    }
+    
 }
