885224ab5131f83e5351109cb181f6162f08cf02
Cameron Grant <cameron.grant2611@gmail.com>
2021-06-28 19:45:41 +0100

    Update java multithreading reference

--- Java/Multithreading/ExtendingThreadExample.java
+++ Java/Multithreading/ExtendingThreadExample.java
@@ -1,42 +1,42 @@
 public class ExtendingThreadExample extends Thread {
 
-	private Thread thread;
-	private String threadName;
+    private Thread thread;
+    private String threadName;
 
-	public ExtendingThreadExample(String threadName) {
-		this.threadName = threadName;
-		System.out.println("Created " + this.threadName);
-	}
+    public ExtendingThreadExample(String threadName) {
+        this.threadName = threadName;
+        System.out.println("Created " + this.threadName);
+    }
 
-	public String getThreadName() {
-		return threadName;
-	}
+    public String getThreadName() {
+        return threadName;
+    }
 
 
-	public void start() {
-		System.out.println("Starting " + this.threadName + ": ");
-		if (thread == null) {
-			thread = new Thread(this, this.threadName);
+    public void start() {
+        System.out.println("Starting " + this.threadName + ": ");
+        if (thread == null) {
+            thread = new Thread(this, this.threadName);
 
-			// calls run() method
-			thread.start();
-		}
-	}
+            // calls run() method
+            thread.start();
+        }
+    }
 
-	@Override
-	public void run() {
-		try {
-			for(int i = 3; i > 0; i--) {
-				System.out.println("Thread " + this.threadName + ": " + i);
+    @Override
+    public void run() {
+        try {
+            for(int i = 3; i > 0; i--) {
+                System.out.println("Thread " + this.threadName + ": " + i);
 
-				Thread.sleep(1000);
-			}
-		} catch (InterruptedException e) {
-			// exception triggered when thread interrupted
-			e.printStackTrace();
-		}
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            // exception triggered when thread interrupted
+            e.printStackTrace();
+        }
 
-		System.out.println("Thread " + this.threadName + " exited");
-	}
+        System.out.println("Thread " + this.threadName + " exited");
+    }
 
 }
--- Java/Multithreading/MultithreadingMain.java
+++ Java/Multithreading/MultithreadingMain.java
@@ -1,24 +1,26 @@
 public class MultithreadingMain {
-	public static void main(String[] args) {
-		RunnableExample runnable1 = new RunnableExample("Runnable Thread 1");
-		RunnableExample runnable2 = new RunnableExample("Runnable Thread 2");
-		ExtendingThreadExample extendingThread1 = new ExtendingThreadExample("Extending Thread 1");
-		ExtendingThreadExample extendingThread2 = new ExtendingThreadExample("Extending Thread 2");
 
-		runnable1.start();
-		runnable2.start();
-		extendingThread1.start();
-		extendingThread2.start();
+    public static void main(String[] args) {
+        RunnableExample runnable1 = new RunnableExample("Runnable Thread 1");
+        RunnableExample runnable2 = new RunnableExample("Runnable Thread 2");
+        ExtendingThreadExample extendingThread1 = new ExtendingThreadExample("Extending Thread 1");
+        ExtendingThreadExample extendingThread2 = new ExtendingThreadExample("Extending Thread 2");
 
-		try {
-			runnable1.getThread().join();
-			runnable2.getThread().join();
-			extendingThread1.join();
-			extendingThread2.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+        runnable1.start();
+        runnable2.start();
+        extendingThread1.start();
+        extendingThread2.start();
 
-		System.out.println("All threads joined");
-	}
+        try {
+            runnable1.getThread().join();
+            runnable2.getThread().join();
+            extendingThread1.join();
+            extendingThread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("All threads joined");
+    }
+    
 }
