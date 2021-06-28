d694327ea804e46f2e1cb173246debdf758e7725
Cameron Grant <cameron.grant2611@gmail.com>
2021-06-28 19:47:21 +0100

    Update java serialization reference

--- Java/Serialization/SerializableClass.java
+++ Java/Serialization/SerializableClass.java
@@ -1,38 +1,39 @@
 import java.io.Serializable;
 
 public class SerializableClass implements Serializable {
-	private int intValue;
-	private String stringValue;
 
-	// transient fields will not be serialized
-	private transient int transientInt;
-	private transient String transientString;
+    private int intValue;
+    private String stringValue;
 
-	// final transient will get serialized
-	public final transient int finalTransientInt = 10;
-	public final transient String finalTransientString = "final transient string";
+    // transient fields will not be serialized
+    private transient int transientInt;
+    private transient String transientString;
 
-	public SerializableClass(int intValue, String stringValue, int transientInt, String transientString) {
-		this.intValue = intValue;
-		this.stringValue = stringValue;
-		this.transientInt = transientInt;
-		this.transientString = transientString;
-	}
+    // final transient will get serialized
+    public final transient int finalTransientInt = 10;
+    public final transient String finalTransientString = "final transient string";
 
-	public int getIntValue() {
-		return intValue;
-	}
+    public SerializableClass(int intValue, String stringValue, int transientInt, String transientString) {
+        this.intValue = intValue;
+        this.stringValue = stringValue;
+        this.transientInt = transientInt;
+        this.transientString = transientString;
+    }
 
-	public String getStringValue() {
-		return stringValue;
-	}
+    public int getIntValue() {
+        return intValue;
+    }
 
-	public int getTransientInt() {
-		return transientInt;
-	}
+    public String getStringValue() {
+        return stringValue;
+    }
 
-	public String getTransientString() {
-		return transientString;
-	}
+    public int getTransientInt() {
+        return transientInt;
+    }
+
+    public String getTransientString() {
+        return transientString;
+    }
 
 }
--- Java/Serialization/SerializationExample.java
+++ Java/Serialization/SerializationExample.java
@@ -8,66 +8,67 @@ import java.io.ObjectOutputStream;
 
 public class SerializationExample {
 
-	public static void serializeObject(SerializableClass object, File file) {
-		try {
-			if(!file.exists()) {
-				file.createNewFile();
-			}
+    public static void serializeObject(SerializableClass object, File file) {
+        try {
+            if(!file.exists()) {
+                file.createNewFile();
+            }
 
-			ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file));
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file));
 
-			objectOutputStream.writeObject(object);
+            objectOutputStream.writeObject(object);
 
-			objectOutputStream.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+            objectOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static SerializableClass deserializeObject(File file) {
-		SerializableClass object = null;
+    public static SerializableClass deserializeObject(File file) {
+        SerializableClass object = null;
 
-		try {
-			ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(file));
+        try {
+            ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(file));
 
-			object = (SerializableClass) inputStream.readObject();
+            object = (SerializableClass) inputStream.readObject();
 
-			inputStream.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
+            inputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
-		return object;
-	}
+        return object;
+    }
 
-	public static void printFields(SerializableClass object) {
-		System.out.println("intValue = " + object.getIntValue());
-		System.out.println("stringValue = " + object.getStringValue());
-		System.out.println("transientInt = " + object.getTransientInt());
-		System.out.println("transientString = " + object.getTransientString());
-		System.out.println("finalTransientInt = " + object.finalTransientInt);
-		System.out.println("finalTransientString = " + object.finalTransientString);
-	}
+    public static void printFields(SerializableClass object) {
+        System.out.println("intValue = " + object.getIntValue());
+        System.out.println("stringValue = " + object.getStringValue());
+        System.out.println("transientInt = " + object.getTransientInt());
+        System.out.println("transientString = " + object.getTransientString());
+        System.out.println("finalTransientInt = " + object.finalTransientInt);
+        System.out.println("finalTransientString = " + object.finalTransientString);
+    }
 
-	public static void main(String[] args) {
-		File outputFile = new File("./serialized_object.bin");
+    public static void main(String[] args) {
+        File outputFile = new File("./serialized_object.bin");
 
-		SerializableClass serializableObject = new SerializableClass(3, "string", 5, "transient string");
+        SerializableClass serializableObject = new SerializableClass(3, "string", 5, "transient string");
 
-		System.out.println("Before Serialization: ");
-		printFields(serializableObject);
+        System.out.println("Before Serialization: ");
+        printFields(serializableObject);
 
-		serializeObject(serializableObject, outputFile);
+        serializeObject(serializableObject, outputFile);
 
-		SerializableClass objectRead = deserializeObject(outputFile);
+        SerializableClass objectRead = deserializeObject(outputFile);
 
-		System.out.println("\nAfter Serialization: ");
-		printFields(objectRead);
-	}
+        System.out.println("\nAfter Serialization: ");
+        printFields(objectRead);
+    }
+    
 }
