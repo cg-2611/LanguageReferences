fc6400342e7070fcc6e8120c92dbce081be84610
Cameron Grant <cameron.grant2611@gmail.com>
2021-06-28 18:55:02 +0100

    Update java gradle references

--- Java/Gradle/LocalJarGradleExample/src/test/java/example/AppTest.java
+++ Java/Gradle/LocalJarGradleExample/src/test/java/example/AppTest.java
@@ -4,8 +4,9 @@ package example;
 // import static org.junit.Assert.*;
 
 public class AppTest {
-	// @Test public void appHasAGreeting() {
-	//     App classUnderTest = new App();
-	//     assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-	// }
+    // @Test
+    // public void appHasAGreeting() {
+    //     App classUnderTest = new App();
+    //     assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    // }
 }
