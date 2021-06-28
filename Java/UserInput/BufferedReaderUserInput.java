03c0e8916a630a99c7d555b846557ef17fadf040
Cameron Grant <cameron.grant2611@gmail.com>
2021-06-28 19:48:18 +0100

    Update java user input reference

--- Java/UserInput/BufferedReaderUserInput.java
+++ Java/UserInput/BufferedReaderUserInput.java
@@ -3,29 +3,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BufferedReaderUserInput {
-	public static void main(String[] args) {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		try {
-			String userInputString;
-			int userInputInteger;
+        try {
+            String userInputString;
+            int userInputInteger;
 
-			System.out.print("Enter string: ");
-			userInputString = br.readLine();
+            System.out.print("Enter string: ");
+            userInputString = br.readLine();
 
-			System.out.print("Enter integer: ");
-			userInputInteger = Integer.parseInt(br.readLine());
+            System.out.print("Enter integer: ");
+            userInputInteger = Integer.parseInt(br.readLine());
 
-			System.out.println("String entered: " + userInputString);
-			System.out.println("Integer entered: " + userInputInteger);
+            System.out.println("String entered: " + userInputString);
+            System.out.println("Integer entered: " + userInputInteger);
 
-			try {
-				br.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- Java/UserInput/ScannerUserInput.java
+++ Java/UserInput/ScannerUserInput.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
 
 public class ScannerUserInput {
-	public static void main(String[] args) {
-		Scanner reader = new Scanner(System.in);
-		String userInputString;
-		int userInputInteger;
+    
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String userInputString;
+        int userInputInteger;
 
-		System.out.print("Enter string: ");
-		userInputString = reader.nextLine();
+        System.out.print("Enter string: ");
+        userInputString = reader.nextLine();
 
-		System.out.print("Enter integer: ");
-		userInputInteger = reader.nextInt();
+        System.out.print("Enter integer: ");
+        userInputInteger = reader.nextInt();
 
-		System.out.println("String entered: " + userInputString);
-		System.out.println("Integer entered: " + userInputInteger);
+        System.out.println("String entered: " + userInputString);
+        System.out.println("Integer entered: " + userInputInteger);
 
-		reader.close();
-	}
+        reader.close();
+    }
+
 }
