2b8fcd8701229d025a0dad54f3df39e683cc8bcb
Cameron Grant <cameron.grant2611@gmail.com>
2021-06-28 19:23:32 +0100

    Update javadoc reference

--- Java/Javadoc/JavadocExampleMain.java
+++ Java/Javadoc/JavadocExampleMain.java
@@ -3,15 +3,15 @@
  */
 public class JavadocExampleMain {
 
-	public static void main(String[] args) {
-		JavadocExampleClassOne classOne = new JavadocExampleClassOne(11);
-		JavadocExampleClassTwo classTwo = new JavadocExampleClassTwo(14, "Class 2");
+    public static void main(String[] args) {
+        JavadocExampleClassOne classOne = new JavadocExampleClassOne(11);
+        JavadocExampleClassTwo classTwo = new JavadocExampleClassTwo(14, "Class 2");
 
-		System.out.println(classOne.getValue());
-		System.out.println(classTwo.getValue());
-		System.out.println(classTwo.getName());
-		classTwo.setName("Class Two");
-		System.out.println(classTwo.getName());
-	}
-	
+        System.out.println(classOne.getValue());
+        System.out.println(classTwo.getValue());
+        System.out.println(classTwo.getName());
+        classTwo.setName("Class Two");
+        System.out.println(classTwo.getName());
+    }
+    
 }
