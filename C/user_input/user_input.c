fcb73de82ff69c70a1965a1db9bd5d5bde44b59a
Cameron Grant <cameron.grant2611@gmail.com>
2021-06-30 16:44:38 +0100

    Add c user input reference

---
+++ C/user_input/user_input.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    char character;
+
+    printf("Enter character: ");
+    // can also use scanf("%c", &character);
+    character = getchar();
+
+    // can also use printf("Character entered: %c", character);
+    printf("Character entered: ");
+    putchar(character);
+
+    char string[256];
+    
+    printf("\n\nEnter string: ");
+    fflush(stdin); // flush the standard input buffer
+    // can also use scanf("%s", string);
+    fgets(string, 256, stdin);
+
+    // can also use printf("String entered: %s", string);
+    printf("String entered: ");
+    puts(string);
+
+    int integer;
+
+    printf("Enter integer: ");
+    fflush(stdin);
+    scanf("%d", &integer);
+    printf("Integer entered: %d\n", integer);
+
+    int i;
+    char c;
+    float f;
+    char s[64];
+
+    printf("\nEnter values: ");
+    fflush(stdin);
+    scanf("%d-%c %f, %s", &i, &c, &f, s);
+    printf("Values entered: %d, \'%c\', %f, \"%s\"", i, c, f, s);
+
+    return 0;
+}
