083fad5da5ca48d437a0c088086012fea6e40eca
Cameron Grant <cameron.grant2611@gmail.com>
2021-06-30 15:23:40 +0100

    Add c static variables reference

---
+++ C/static_variables/static_variables.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void function() {
+    int x = 10;
+    static int static_x = 10;
+
+    int i;
+    for (i = 0; i < 5; i++) {
+        printf("x = %d, static_x = %d\n", x++, static_x++);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int i;
+    for (i = 0; i < 5; i++) {
+        function();
+    }
+
+    return 0;
+}
