c5f21c88ae366f6795f4d0797f9ff2ad56a5fd5c
Cameron Grant <cameron.grant2611@gmail.com>
2021-06-30 15:19:55 +0100

    Update c command line arguments reference

--- C/command_line_arguments/command_line_arguments.c
+++ C/command_line_arguments/command_line_arguments.c
@@ -5,7 +5,8 @@ int main(int argc, char *argv[]) {
     
     printf("Number of arguments passed: %d\n\n", argc);
 
-    for (int i = 0; i < argc; i++) {
+    int i;
+    for (i = 0; i < argc; i++) {
         printf("Argument %d: %s\n", (i + 1), argv[i]);
     }
 
