8fea56113b8cf51d1f712c2d8041c1a70beb7ecc
Cameron Grant <cameron.grant2611@gmail.com>
2021-06-30 12:26:51 +0100

    Add c command line arguments reference

---
+++ C/command_line_arguments/command_line_arguments.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    // argc always >= 1 as program name is counted
+    
+    printf("Number of arguments passed: %d\n\n", argc);
+
+    for (int i = 0; i < argc; i++) {
+        printf("Argument %d: %s\n", (i + 1), argv[i]);
+    }
+
+    printf("\nFirst argument provided: %s\n", argv[0]);
+    printf("Final argument provided: %s\n", argv[argc - 1]);
+
+    return 0;
+}
