70fc2f6ce263b6c7d99f628a3917c91ec8490102
Cameron Grant <cameron.grant2611@gmail.com>
2021-07-11 20:49:40 +0100

    Improve c dynamic strings reference

--- C/strings/dynamic_strings.c
+++ C/strings/dynamic_strings.c
@@ -10,17 +10,19 @@ int main(int argc, char *argv[])
     // char string[];
 
     // example:
-    char *dynamic_string = (char *)malloc((sizeof(char) * 16) + 1); // allocate memory for a string of size 16 + 1
+    char *dynamic_string = malloc((16 + 1) * sizeof(*dynamic_string)); // allocate memory for a string of size 16 + 1
     strcpy(dynamic_string, "This is a string"); // copy a string to the memory
 
     printf("Dynamic string value: %s\n", dynamic_string);
     printf("Dynamic string length: %lu\n", strlen(dynamic_string));
 
+    // can also use realloc instead of free and malloc
+
     // deallocate memory
     free(dynamic_string);
     dynamic_string = NULL;
 
-    dynamic_string = (char *)malloc((sizeof(char) * 32) + 1); // allocate memory for a string of size 32 + 1
+    dynamic_string = malloc((32 + 1) * sizeof(*dynamic_string)); // allocate memory for a string of size 32 + 1
     strcpy(dynamic_string, "This is a bigger string"); // copy a larger string to the memory
 
     printf("Dynamic string value: %s\n", dynamic_string);
