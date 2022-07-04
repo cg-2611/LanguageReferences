28d31955800ce9b8b590fa28c4ae6202a6c58fc9
Cameron Grant <cameron.grant2611@gmail.com>
2022-07-04 17:40:46 +0100

    add c++ insertion sort reference

---
+++ C++/sorting/insertion_sort.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+void insertion_sort(std::vector<int> array);
+void print_array(std::vector<int> array);
+
+void insertion_sort(std::vector<int> array)
+{
+    for (size_t i = 1; i < array.size(); i++)
+    {
+        int element_to_insert = array[i];
+        int previous = i - 1;
+
+        while ((previous >= 0) && (array[previous] > element_to_insert))
+        {
+            array[previous + 1] = array[previous];
+            previous--;
+        }
+
+        array[previous + 1] = element_to_insert;
+        print_array(array);
+    }
+}
+
+void print_array(std::vector<int>array)
+{
+    std::cout << "[";
+    for(size_t i = 0; i < array.size() - 1; i++)
+    {
+        std::cout << array[i] << ", ";
+    }
+    std::cout <<  array[array.size() - 1] << "]" << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+    std::vector<int> unsorted_array1 = {9, 3, 2, 7, 1, 4, 5, 8, 6};
+    std::cout << "Unsorted Array 1:" << std::endl;
+    print_array(unsorted_array1);
+
+    std::cout << "\nInsertion Sort:" << std::endl;
+    insertion_sort(unsorted_array1);
+
+    return 0;
+}
