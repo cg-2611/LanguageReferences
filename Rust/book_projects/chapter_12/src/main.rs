a2a1904725c340018df0298167eb3910a962088f
Cameron Grant <cameron.grant2611@gmail.com>
2022-09-29 21:43:03 +0100

    update rust chapter 12 book project

--- Rust/book_projects/chapter_12/src/main.rs
+++ Rust/book_projects/chapter_12/src/main.rs
@@ -4,10 +4,8 @@ use std::process;
 use chapter_12::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config: Config = Config::build(&args).unwrap_or_else(|err: &str| {
-        eprintln!("error parsing arguments: {err}");
+    let config: Config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
